37d3a13af8dbc40ae4ad5ee306352b75181e021b
kmaria <katona.maria@gmail.com>
2017-06-03 18:24:22 +0200

    Set Allowed Hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@+_f)y5b)xi%o6s2+z(%$i5z54a7b&)c5*yj*m$x-@ib(nans5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['kmaria.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', 'kmaria.pythonanywhere.com']
 
 
 # Application definition
